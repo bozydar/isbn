0d4af8b0f2af735ae310c9fdcddcc4206826135b
Bozydar Sobczak <bozysob@gmail.com>
2024-02-01 12:44:48 +1300

    Refact: Applying better names and fixing warnings

--- lib/isbn.rb
+++ lib/isbn.rb
@@ -1,13 +1,13 @@
 class Isbn
-  def initialize(isbn12:)
-    raise ArgumentError.new("isbn12 is invalid") if isbn12.length != 12
+  def initialize(prefix12:)
+    raise ArgumentError.new("prefix12 is invalid") if prefix12.length != 12
 
-    @isbn12 = isbn12
+    @prefix12 = prefix12
   end
 
   def check_digit
     result = 0
-    @isbn12.split('').each_with_index do |value, index|
+    @prefix12.split('').each_with_index do |value, index|
       m = index % 2 == 0 ? 1 : 3
       result += value.to_i * m
     end
--- spec/isbn_spec.rb
+++ spec/isbn_spec.rb
@@ -5,21 +5,21 @@ require "isbn"
 describe "Isbn" do
   describe "#check_digit" do
     it "returns proper value" do
-      isbn = Isbn.new(isbn12: "978014300723")
-      isbn.check_digit.must_equal 4
+      isbn = Isbn.new(prefix12: "978014300723")
+      _(isbn.check_digit).must_equal 4
 
-      isbn = Isbn.new(isbn12: "978014300724")
-      isbn.check_digit.must_equal 1
+      isbn = Isbn.new(prefix12: "978014300724")
+      _(isbn.check_digit).must_equal 1
 
-      isbn = Isbn.new(isbn12: "978014300734")
-      isbn.check_digit.must_equal 0
+      isbn = Isbn.new(prefix12: "978014300734")
+      _(isbn.check_digit).must_equal 0
     end
   end
 
   describe ".initialize" do
     describe "when isbn12 is wrong" do
       it "raises the exception" do
-        proc { Isbn.new(isbn12: "1234567890") }.must_raise ArgumentError
+        _{ Isbn.new(prefix12: "1234567890") }.must_raise ArgumentError
       end
     end
   end
